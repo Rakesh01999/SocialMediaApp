528e310108cb47160812094b89bc544d557e4c81
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-06 09:39:50 +0600

    header component added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -13,21 +14,28 @@ import {
 import Title from './components/Title/Title';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import style from './assets/styles/main';
 
 const App = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-        <View style={{
-          paddingTop:30,
-          paddingLeft:17,
-          paddingRight:26,
-          flex:1, 
-          flexDirection:'row', 
-          alignItems:'center',
-          justifyContent: 'space-between'}}>
+        <View style={style.header}>
           <Title title={"Let's Explore"} />
-          <FontAwesomeIcon icon={faEnvelope} />
+          <Pressable style={style.messageIcon}>
+            <FontAwesomeIcon icon={faEnvelope} color={'#CACDDE'} size={20}  />
+            <View style={style.messageNumberContainer}>
+              <Text style={{
+                fontSize: 6,
+                fontFamily: 'Inter',
+                lineHeight: 7,
+                fontWeight: 600,
+                color:'#FFFFFF'
+              }}>
+                2
+              </Text>
+            </View>
+          </Pressable>
         </View>
       </ScrollView>
     </SafeAreaView>
